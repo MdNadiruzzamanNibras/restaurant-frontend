c3fe55cf4cf032cedb8127ac4e7369f0bd365ef5
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-07-14 15:44:38 +0600

    router set

---
+++ src/Home/Home.jsx
@@ -0,0 +1,14 @@
+
+
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="row">
+            <p className="fs-1">kid</p>
+            </div>
+            <p className="fs-1">kid</p>
+        </div>
+    );
+};
+
+export default Home;
